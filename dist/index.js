96aa1db3cb0dcb7cfdab97e262cdad70efbd77bc
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-02-22 16:11:27 -0300

    update

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const puppeteer_1 = __importDefault(require("puppeteer"));
+(async () => {
+    const browser = await puppeteer_1.default.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({ path: 'example.png' });
+    await browser.close();
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const a: number = 1
-console.log(a)
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  console.log(browser)
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
